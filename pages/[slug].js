9c6dd30d240910489f55ec0769d9d201df7518f1
Nazar Maksymchuk <nzrbeats@gmail.com>
2021-03-29 00:20:32 -0700

    dark mode

--- pages/[slug].js
+++ pages/[slug].js
@@ -24,13 +24,6 @@ export const POST_QUERY = gql`
 `;
 
 export default function Page({ initialData }) {
-  // const { loading, error, data = {} } = useQuery(PORTFOLIO_QUERY);
-  // const {
-  //   blogPostCollection: posts = { items: [], total: 0 },
-  //   skillCollection: skills = { items: [], total: 0 },
-  //   projectCollection: projects = { items: [], total: 0 },
-  //   workHistoryCollection: workHistory = { items: [], total: 0 },
-  // } = initialData;
   const { blogPostCollection } = initialData;
 
   return (
@@ -40,17 +33,19 @@ export default function Page({ initialData }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="container mx-auto block py-4 grid grid-cols-5 gap-4">
-        <h1 className="inline col-span-5 md:col-span-1 text-2xl font-bold hover:bg-yellow-200 cursor-pointer">
+      <header className="container mx-auto block py-4 grid grid-cols-4 gap-4">
+        <h1 className="inline col-span-5 md:col-span-1 text-2xl font-bold">
           <Link href="/">
-            <a>Nazar Maksymchuk</a>
+            <a className="hover:bg-yellow-200 dark:hover:text-black cursor-pointer">
+              Nazar Maksymchuk
+            </a>
           </Link>
         </h1>
-        <div className="col-span-3">Links</div>
+        <div className="col-span-2">Links</div>
         <div className="col-span-1 text-right">GITHUB</div>
       </header>
       <div className="container mx-auto mb-20">
-        <div className="row grid grid-cols-4 gap-4">
+        <div className="row grid grid-cols-4 gap-4 mt-8">
           <div className="col-span-4 lg:col-span-1">
             {/* <h2 className="text-xl font-bold mb-4">Hustles</h2>
             {projects.items.map((skill) => (
@@ -70,11 +65,14 @@ export default function Page({ initialData }) {
             ))} */}
           </div>
           <div className="col-span-4 lg:col-span-2">
+            <Link href="/">
+              <a>{"<-"} Posts</a>
+            </Link>
             <h2 className="text-4xl font-bold mb-3">
               {blogPostCollection.items[0].title}
             </h2>
             <div className="attributes mb-3">
-              <span className="text-xl font-bold mb-3 text-gray-700 ">
+              <span className="text-xl font-bold mb-3 text-gray-700 dark:text-gray-400">
                 {"Published on " +
                   format(
                     new Date(blogPostCollection.items[0].publishDate),
@@ -142,14 +140,13 @@ export default function Page({ initialData }) {
 
 export async function getStaticProps({ params }) {
   const data = await fetcher(POST_QUERY, { slug: params.slug });
-  // console.log("data", data);
-  // when no typee of page is not found return 404 page
+
   return {
     props: {
       initialData: data,
     },
     revalidate: 60,
-    // notFound: true
+    notFound: !data,
   };
 }
 
