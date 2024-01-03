b741382bba1ea712b7af1c249beb9eeda21def2a
Nazar Maksymchuk <nazar.maksymchuk@calpers.ca.gov>
2024-01-02 23:32:51 -0800

    google ads tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,22 @@ import { Footer } from "../components/footer";
 import { useSpring, animated } from "@react-spring/web";
 
 import "../styles/globals.css";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-1EDCDHVLR7">
+      </Script>
+      <Script dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-1EDCDHVLR7');
+        `}} />
+
       <body className="p-4 2xl:p-0 bg-white dark:bg-gray-800 dark:text-gray-300 transition-all duration-300	ease-in-out">
         <ThemeProvider attribute="class">
           <ApolloClientProvider>
