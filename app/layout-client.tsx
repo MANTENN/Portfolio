1ac5ca04951c57ac71e24dab9bbda32ef44a8dac
Nazar Maksymchuk <nzrbeats@gmail.com>
2024-01-03 11:36:12 -0800

    removed unnecessary split bee code

--- app/layout-client.tsx
+++ app/layout-client.tsx
@@ -1,14 +1,9 @@
 "use client";
+import { split } from "@apollo/client";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useEffect } from "react";
 
 export default function ThemeProvider({ children, ...props }) {
-  useEffect(() => {
-    // @ts-ignore: Unreachable code error
-    splitbee.init({
-      scriptUrl: "/bee.js",
-      apiUrl: "/_hive",
-    })
-  }, [])
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
