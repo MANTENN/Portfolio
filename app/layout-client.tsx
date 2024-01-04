6fba78f20fec0222c49e2b10137b86eabcf6a1b2
Nazar Maksymchuk <nzrbeats@gmail.com>
2024-01-03 17:40:35 -0800

    pixel build error patch

--- app/layout-client.tsx
+++ app/layout-client.tsx
@@ -5,17 +5,29 @@ import { useEffect } from "react";
 
 import { usePathname } from "next/navigation";
 
-const ReactPixel = import('react-facebook-pixel').then(x => x.default)
+const ReactPixel = import('react-facebook-pixel').then(x => x.default).catch(e => {
+  console.log('error importing facebook pixel\n\n', e)
+  return {};
+})
 
 export default function ThemeProvider({ children, ...props }) {
   useEffect(() => {
+
     ReactPixel.then((ReactPixel) => {
+      // @ts-ignore
       ReactPixel.init(process.env.PIXEL_ID!)
-    })
+    }).catch(e => console.log('error initializing facebook pixel:\n\n', e))
+
   }, [])
+
   const pathname = usePathname()
   useEffect(() => {
-    ReactPixel.then(ReactPixel => ReactPixel.pageView())
+    ReactPixel.then(ReactPixel => {
+      // @ts-ignore
+      ReactPixel.pageView()
+    }).catch(e => {
+      console.log('error tracking pageView:\n\n', e)
+    })
   }, [pathname])
 
 
