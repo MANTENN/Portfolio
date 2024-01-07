a80009c4c6a397205f1664ebc5704444c6684315
Nazar Maksymchuk <nzrbeats@gmail.com>
2024-01-06 23:04:04 -0800

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
 import { NextResponse, type NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  console.log(request.headers.get('x-vercel-country-ip'), request.headers.get('x-vercel-country-ip'))
-  if (request.headers.get('x-vercel-country-ip') !== "US") {
+  if (process.env.NODE_ENV !== 'production' && request.headers.get('x-vercel-country-ip') !== "US") {
     return NextResponse.json(null, { status: 500 })
   }
   return NextResponse.next()
