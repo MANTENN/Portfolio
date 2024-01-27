2c33a03ab42b1ea2b401acc348e4b07a2c62bb9b
Nazar Maksymchuk <nzrbeats@gmail.com>
2024-01-26 18:21:33 -0800

    updated single page, and created hero and section components

--- components/hero/index.js
+++ components/hero/index.js
@@ -1,14 +1,14 @@
 import { CallToAction } from "./call-to-action";
 
-export function Hero() {
+export function Hero({ hook = "Stop wasting your time. Excel at your speciality." }) {
   return (
     <div className="flex items-center h-full py-16">
       <div className="max-w-2xl">
-        <span className="block text-green-700 dark:text-green-500 font-bold text-4xl leading-8 mb-6">
-          Stop wasting your time. Excel at your speciality.
+        <span className="block font-bold text-4xl leading-9 mb-6">
+          {hook}
         </span>
-        <div className="flex gap-2 items-center mb-8">
-          <CallToAction />
+        <div className="flex-col md:flex-row gap-2 md:items-center mb-8 hidden md:flex">
+          <CallToAction />``
           <a href="tel:279-218-0833" className="flex items-center mx-4">(279)-218-0833</a>
         </div>
       </div>
